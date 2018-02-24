02a939640aad1e2ac6a58a43bab126a137aa8153
brian <brianc.cb@gmail.com>
2018-02-24 14:59:56 -0500

    Created Artist model and db schema

---
+++ db/migrate/20180224195251_create_artists.rb
@@ -0,0 +1,11 @@
+class CreateArtists < ActiveRecord::Migration[5.0]
+  def change
+    create_table :artists do |t|
+      t.string :name, null: false
+
+      t.timestamps
+    end
+  end
+
+  add_index :artists, :name
+end
